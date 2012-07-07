656483358d123478b08fc64f8d11a4244adec0d7
Nigel Kersten <nigel@explanatorygap.net>
2012-07-07 09:13:53 -0700

    whatever

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,8 +1,22 @@
-setopt promptsubst
-autoload -U colors && colors
-autoload -U promptinit
-promptinit
+setopt PROMPT_SUBST
 
-precmd() { print "$(puppetversion) $(vmfusionlist)" }
-PS1="%{$fg[red]%}%n%{$reset_color%}@%{$fg[blue]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+autoload -U colors && colors
+autoload -U promptinit && promptinit
+
+#function precmd() { print "$fg[blue]$(puppetversion)$reset_color $fg[yellow]$(vmfusionlist)$reset_color" }
+
+function vmfusionlist() {
+  local vms=$(vmrun list | gawk 'BEGIN { ORS=":" ; FS="/" }  { if (NR>1) {sub(/.vmx/, "", $NF) ; print $NF} }')
+  echo ${vms%%:}
+}
+
+function puppetversion() {
+  gawk 'BEGIN { ORS=" " } /PUPPETVERSION =/ { gsub("\047", "") ; printf $3 }' ~/src/puppet/lib/puppet.rb
+}
+
+
+PROMPT="%{$fg[white]%}%n@%m %{$fg[blue]%}%~ %{$reset_color%}% 
+%{$fg[white]%}[%{$fg[black]%}%h%{$fg[white]%}]%{$fg[black]%}%# "
+
+RPROMPT="%{$reset_color%}%{$fg[white]%}[%{$fg[magenta]%}$(vmfusionlist)%{$fg[white]%}][%{$fg[magenta]%}$(puppetversion)%{$fg[white]%}]%{$reset_color%}"
 
