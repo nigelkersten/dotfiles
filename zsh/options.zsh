471fd8af9efe3e791d14df68fca53a072ddf7ff5
Nigel Kersten <nigel@explanatorygap.net>
2014-07-28 20:59:27 -0700

    set up antigen

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,6 +1,9 @@
 # shell state
 setopt nobeep
 
+# chdir
+setopt pushd_ignore_dups
+
 # prompt
 setopt prompt_subst
 setopt nopromptcr
@@ -15,3 +18,9 @@ setopt extended_history
 setopt hist_ignore_space
 unsetopt histverify
 setopt hist_ignore_dups
+
+# completion
+setopt always_to_end
+setopt auto_menu
+setopt auto_name_dirs
+setopt complete_in_word
